dac2e32f2b616b23822f17d8e68fc768f75c5de8
Dyl6875 <132678439+Dyl6875@users.noreply.github.com>
2023-05-05 17:21:14 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("2")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+})
+basic.showIcon(IconNames.Ghost)
